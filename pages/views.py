6059775051ec379961b8f5ecf2eae6cb1a0a8c11
ruschakartur2 <ruschakartur@gmail.com>
2020-07-31 01:23:15 +0300

    Added about and home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("Hello world")
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
